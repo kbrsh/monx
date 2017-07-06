2b0bf56fdebae2fb86a947643115801210e0e4a6
Kabir Shah <simplecooldude1@gmail.com>
2017-07-06 16:23:38 -0700

    add reactivity

--- dist/monx.js
+++ dist/monx.js
@@ -9,8 +9,66 @@
   /* ======= Global Monx ======= */
   (typeof module === "object" && module.exports) ? module.exports = factory() : root.Monx = factory();
 }(this, function() {
-    function Monx(options) {
+    var Moon = null;
     
+    var defineProperty = function(obj, prop, value, def) {
+      if(value === undefined) {
+        obj[prop] = def;
+      } else {
+        obj[prop] = value;
+      }
+    }
+    
+    
+    function Monx(options) {
+      // Setup state
+      this.state = {};
+      defineProperty(this, "_state", options.state, {});
+    
+      // Setup actions
+      defineProperty(this, "actions", options.actions, {});
+    
+      // Setup instance
+      this.instance = null;
+    }
+    
+    Monx.prototype.dispatch = function(name, payload) {
+      this.actions[name](this.state, payload);
+    }
+    
+    Monx.prototype.install = function(instance) {
+      this.instance = instance;
+    
+      var state = this.state;
+      var _state = this._state;
+      var loop = function ( key ) {
+        Object.defineProperty(state, key, {
+          get: function() {
+            return _state[key];
+          },
+          set: function(value) {
+            _state[key] = value;
+            instance.build();
+          }
+        });
+      };
+    
+      for(var key in _state) loop( key );
+    }
+    
+    Monx.init = function(_Moon) {
+      Moon = _Moon;
+    
+      var MoonInit = Moon.prototype.init;
+      Moon.prototype.init = function() {
+        if(this.$options.store !== undefined) {
+          var store = this.$options.store;
+          this.$data.store = store;
+          store.install(this);
+        }
+    
+        MoonInit.apply(this, arguments);
+      }
     }
     
     return Monx;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
-function Monx(options) {
+let Moon = null;
 
+//=require ./util/util.js
+
+function Monx(options) {
+  // Setup state
+  this.state = {};
+  defineProperty(this, "_state", options.state, {});
+
+  // Setup actions
+  defineProperty(this, "actions", options.actions, {});
+
+  // Setup instance
+  this.instance = null;
+}
+
+Monx.prototype.dispatch = function(name, payload) {
+  this.actions[name](this.state, payload);
+}
+
+Monx.prototype.install = function(instance) {
+  this.instance = instance;
+
+  let state = this.state;
+  let _state = this._state;
+  for(let key in _state) {
+    Object.defineProperty(state, key, {
+      get: function() {
+        return _state[key];
+      },
+      set: function(value) {
+        _state[key] = value;
+        instance.build();
+      }
+    });
+  }
+}
+
+Monx.init = function(_Moon) {
+  Moon = _Moon;
+
+  let MoonInit = Moon.prototype.init;
+  Moon.prototype.init = function() {
+    if(this.$options.store !== undefined) {
+      let store = this.$options.store;
+      this.$data.store = store;
+      store.install(this);
+    }
+
+    MoonInit.apply(this, arguments);
+  }
 }
